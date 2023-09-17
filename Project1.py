71d87df8a30452305b9c0861d9be0e6b54c03ac9
emily7lim <70532210+emily7lim@users.noreply.github.com>
2023-09-17 08:39:43 +0000

    modify key comparison

--- Project1.py
+++ Project1.py
@@ -3,29 +3,34 @@ import time
 
 def hybridSort(L,S): #input array L and threshold S as parameters
     size=len(L)
+    kc=0
     if size>S: #merge
         if size>1: #minimum size array atleast two elements
-            L1 = L[:(size)//2]
-            L2= L[size//2:]
             #divide into 2 equal sized sub arrays or problems
             #find solution to each of these sub problems
-            if (len(L1)<=S):
+            L1 = L[:(size)//2]
+            L2= L[size//2:]
+
+            if (len(L1) <= S):
                 #switch to insertion sort
-               insertion(L1)
-            if (len(L2)<=S):
-                insertion(L2)
-                        
-            if (len(L1)>S ) :
+               kc+=insertion(L1)
+               print(kc,"II")
+            if (len(L2) <= S):
+                kc+=insertion(L2)
+                print(kc,"&")
+            if (len(L1) > S):
                 L1 = hybridSort(L1,len(L1))
-            if (len(L2)>S):
+            if (len(L2) > S):
                 L2 = hybridSort(L2,len(L2))
             
-            L=merge(L1,L2)
+            L,keys=merge(L1,L2)
+            kc+=keys
         else:
             return
     else: # insertion
-        insertion(L)
-    return L
+        kc+=insertion(L)
+    # print(kc)
+    return L,kc
 
 def insertion(L):
     key_comparison = 0
@@ -39,34 +44,30 @@ def insertion(L):
     return key_comparison
 
 def merge(L1,L2):
-    L=[]
+    L=[] #temp arr to str sorted
     kc=0
     while (L1 != [] and L2 != []):
         kc+=1
         if(L1[0]<L2[0]):
-            L.append(L1[0])
-            L1.remove(L1[0])
-            print(L1,L2)
+            modifyArray(L,L1)
         elif(L2[0]<L1[0]):
-            L.append(L2[0])
-            L2.remove(L2[0])
+            modifyArray(L,L2)
         else: #the 1st element of 2 halves are equal
             if(len(L1)==1 or len(L2)==1): break
-            L.append(L1[0])
-            L.append(L2[0])
-            L1.remove(L1[0])
-            L2.remove(L2[0])
+            modifyArray(L,L1)
+            modifyArray(L,L2)
     if(L1==[]):
-        # L.append(L1[0])
-        L.append(L2[0])
-        # L1.remove(L1[0])
-        L2.remove(L2[0])
+        modifyArray(L,L2)
     elif (L2==[]):
-        L.append(L1[0])
-        L1.remove(L1[0])
+        modifyArray(L,L1)
+    return L,kc
 
-    return L
+def modifyArray(L, arr):
+    L.append(arr[0])
+    arr.remove(arr[0])
 
-arr = [14,40,28,31,3,15,17,51]
-print(hybridSort(arr,1))
+# arr = [14,40,28,31,3,15,17,51]
+arr = [22,1,3,2,6,8,9]
+print(hybridSort(arr,5))
+# print(insertion([3,5,1,6,2]))
 # print(merge([14,40,28,31],[3,15,17,51]))
