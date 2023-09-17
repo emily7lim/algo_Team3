7a2b6f290ea9d742e6ffdfc14c6a615d6f20b3a1
emily7lim <70532210+emily7lim@users.noreply.github.com>
2023-09-17 10:32:23 +0000

    edit orig mergesort

--- mergesort.py
+++ mergesort.py
@@ -1,72 +1,39 @@
-import time
-import random
+def merge(L1,L2):
+    L=[] #temp arr to str sorted
+    # kc=0
+    while (L1 != [] and L2 != []):
+        # kc+=1
+        if(L1[0]<L2[0]):
+            modifyArray(L,L1)
+        elif(L2[0]<L1[0]):
+            modifyArray(L,L2)
+        else: #the 1st element of 2 halves are equal
+            modifyArray(L,L1)
+            modifyArray(L,L2)
+    while (L1 != [] or L2 != []):
+        if(L1==[]):
+            modifyArray(L,L2)
+        elif (L2==[]):
+            modifyArray(L,L1)
+    # return L,kc
+    return L
+
+def modifyArray(L, arr):
+    L.append(arr[0])
+    arr.remove(arr[0])
+
+def MergeSort(L):
+    mid = (len(L)-1)//2 #index to split at
+    if (len(L)==0 or len(L)==1):
+        return L
+    else:
+        x1 =  MergeSort(L[:(mid+1)])
+        x2 =  MergeSort(L[(mid+1):])
+    n=0
+    m= len(L)-1
+
+    return merge(x1,x2)
 
 
-def merge(arr, left, mid, right):
-    n1 = mid - left + 1
-    n2 = right - mid
-
-    # create temp arrays
-    L = [0] * n1
-    R = [0] * n2
-
-    # Copy data to temp arrays L[] and R[]
-    for i in range(0, n1):
-        L[i] = arr[left + i]
-
-    for j in range(0, n2):
-        R[j] = arr[mid + 1 + j]
-
-    i = j = 0
-    k = left
-
-    while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
-            i += 1
-        else:
-            arr[k] = R[j]
-            j += 1
-        k += 1
-
-        # copy any remaining elements of L[]
-    while i < n1:
-        arr[k] = L[i]
-        i += 1
-        k += 1
-
-    # copy any remaining elements of R[]
-    while j < n2:
-        arr[k] = R[j]
-        j += 1
-        k += 1
-
-
-def mergeSort(arr, left, right):
-    if left < right:
-
-        mid = left+(right-left)//2  # get the int
-
-        mergeSort(arr, left, mid)
-        mergeSort(arr, mid+1, right)
-        merge(arr, left, mid, right)
-
-
-# test code
-arr = [random.randint(1, 50) for _ in range(0, 10)]
-arrlen = len(arr)
-print("Given array is")
-for i in range(arrlen):
-    print("%d" % arr[i], end=" ")
-
-mergeSort(arr, 0, arrlen-1)
-print("\n\nSorted array is")
-
-# f = open('output.txt', 'w')
-for i in range(arrlen):
-    print("%d" % arr[i], end=" ")
-#     f.write(str(arr[i]))
-#     f.write("\n")
-# f.close()
-
-  
+arr = [4,23,2,1,4,7,6]
+print(MergeSort(arr))
