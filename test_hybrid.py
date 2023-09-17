b96f00aadc135c9d0648f0a15ab5e43ea6fbf97a
emily7lim <70532210+emily7lim@users.noreply.github.com>
2023-09-17 16:10:23 +0000

    add testcase

--- test_hybrid.py
+++ test_hybrid.py
@@ -14,15 +14,30 @@ def test_insert_kc():
     output = Project1.insertion([3,6,5,2,1,4,10])
     assert output == 14
 
-# def test_merge_kc():
-#     # for merge sort
-#     output = Project1.hybridSort([14,40,28,31,3,15,17,51],threshold_5)
-#     assert output == 9
+def test_merge_kc():
+    # for merge sort
+    output = Project1.merge([5,14,17,51],[1,2,3,20,99])
+    assert output == ([1,2,3,5,14,17,20,51,99],8)
 
-# def test_merg_kc():
-#     # for merge sort
-#     output = Project1.hybridSort([90,25,10,71,94,22,59,74],threshold_5)
-#     assert output == 17
+def test_merg_samei():
+    # for merge sort
+    output = Project1.merge([4],[4])
+    assert output == ([4,4], 1)
+
+def test_merg_same():
+    # for merge sort
+    output = Project1.merge([4,8],[4,8])
+    assert output == ([4,4,8,8], 2)
+
+def test_merge_empty():
+    # for merge sort
+    output = Project1.merge([],[1,2,3,20,99])
+    assert output == ([1,2,3,20,99],0)
+
+def test_merg_empt():
+    # for merge sort
+    output = Project1.merge([2,4,80,90],[])
+    assert output == ([2,4,80,90],0)
 
 # -------------- test for arrays ------------------
 def test_empty():
@@ -63,7 +78,7 @@ def test_duplicat():
     output = Project1.hybridSort([2,1,4,2],threshold_1)
     assert output == ([1,2,2,4],4)
 
-def test_duplicate_all():
+def test_duplicate_al():
     output = Project1.hybridSort([5,5,5,5,5],threshold_1)
     assert output == ([5,5,5,5,5],5)
 
@@ -91,7 +106,17 @@ def test_low_threshold():
 
 
 # -------------- test for original mergesort ------------------
-def test_merge_kc():
+def test_mergesort():
     # for merge sort
-    output = mergesort.MergeSort([14,40,28,31,3,15,17,51],threshold_5)
-    assert output == 9
+    output = mergesort.MergeSort([14,40,28,31,3,15,17,51])
+    assert output == ([3,14,15,17,28,31,40,51],16)
+
+def test_mergesort_odd():
+    # for merge sort
+    output = mergesort.MergeSort([4,23,2,1,5,7,6])
+    assert output == ([1,2,4,5,6,7,23],13)
+
+def test_mergesort_duplicate():
+    # for merge sort
+    output = mergesort.MergeSort([4,23,2,1,4,7,6])
+    assert output == ([1,2,4,4,6,7,23],12)
